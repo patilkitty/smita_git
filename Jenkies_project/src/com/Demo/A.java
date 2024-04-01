6761ff280893f7cf229b93baab4934005665a45f
adhya <adhya@192.168.100.17>
2024-04-01 13:10:50 +0530

    By smita

---
+++ Jenkies_project/src/com/Demo/A.java
@@ -0,0 +1,14 @@
+package com.Demo;
+
+public class A {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Smita pawar");
+	}
+	public void m1()
+	{
+		System.out.println("Jenkien");
+	}
+
+}
